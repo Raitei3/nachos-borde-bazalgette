f8efaac7abce88ad0d33131043956af1746bd063
Raitei3 <antoine24.borde@gmail.com>
2016-10-28 18:18:30 +0200

    création des thread implémenter

--- svnnachos/code/userprog/userthread.h
+++ svnnachos/code/userprog/userthread.h
@@ -1,9 +1,27 @@
+
 #ifdef CHANGED
+
+#include "../threads/thread.h"
+#include "../machine/machine.h"
+#include "addrspace.h"
+#include "system.h"
+#include "syscall.h"
+#include "copyright.h"
+
 
 #ifndef USERTHREAD_H
 #define USERTHREAD_H
 
+typedef struct argInitThread {
+  int fun;
+  int* args;
+} argInitThread ;
+
+
 extern int do_Thread_Create(int f, int arg);
+static void StartUserThread(void * init);
+void do_ThreadExit(Thread * t);
+
 
 #endif // USERTHREAD_H
 #endif // CHANGED
