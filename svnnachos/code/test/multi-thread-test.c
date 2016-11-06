1c9c59c0c94c3a1b6bd8c30151200e5c32528629
Raitei3 <antoine24.borde@gmail.com>
2016-11-06 01:12:12 +0100

    modification du test multi thread pour lancer facilement autan de thread qu'on veut

--- svnnachos/code/test/multi-thread-test.c
+++ svnnachos/code/test/multi-thread-test.c
@@ -1,22 +1,18 @@
 #include "syscall.h"
 
 void func();
-void func2();
-void func3();
-void func4();
 
-char a = 'a';
-char b = 'b';
-char c ='c';
+int nbThread = 3;
+char a = 'T';
+
 
 int main(){
-
-  void * v = &(func);
-  ThreadCreate(v,&b);
-  ThreadCreate(v,&c);
-  //ThreadCreate(v3,0);
-  //ThreadCreate(v4,0);
-  func(&a);
+  void * v = (func);
+  int i;
+  for (i = 0; i < nbThread; i++) {
+    ThreadCreate(v,&a);
+  }
+  ThreadExit();
   return 0;
 }
 
@@ -24,7 +20,7 @@ int main(){
 void func(void* c) {
   char* x = c;
   volatile int i=0;
-  for (i=0; i < 15; i++)
+  for (i=0; i < 3; i++)
     PutChar(*x);
   ThreadExit();
 }
