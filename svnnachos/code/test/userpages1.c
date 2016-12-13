ae3520d6df44e50532c8db21c14b8041d2a6a610
Raitei3 <antoine24.borde@gmail.com>
2016-12-13 19:29:31 +0100

    ajout de test

---
+++ svnnachos/code/test/userpages1.c
@@ -0,0 +1,29 @@
+
+#include "syscall.h"
+#define THIS "a"
+#define THAT "b"
+
+const int N = 10; // Choose it large enough!
+
+void puts(const char * s)
+{
+  const char * p;
+  for (p = s; * p != '\0'; p++)
+  PutChar(* p);
+}
+
+void f(void * arg)
+{
+  const char* s = arg;
+  int i;
+  for (i = 0; i < N; i++)
+  puts(s);
+  //ThreadExit();
+}
+
+int main()
+{
+  ThreadCreate(f, THIS);
+  f(THAT);
+  return 1;
+}
